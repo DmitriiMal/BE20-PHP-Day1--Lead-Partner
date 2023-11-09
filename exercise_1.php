b8b106bd5e7e9cbe12c93874ceaa553c78209fad
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-11-09 10:29:09 +0100

    add style.css and complete Exercise 3

--- exercise_1.php
+++ exercise_1.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
   <title>Document</title>
 </head>
 <body>
@@ -17,6 +18,11 @@
   $age = date("Y") - $birthYear;
 
   echo "Hello, my name is $firstName $lastName and I am $age years old";
+
+  echo "</br>";
+  # Exercise 3
+  $player_array = ["Mark", "John", "Georg", "Lisa"];
+  echo "The third player in the team is $player_array[2]"
   ?>
 </body>
 </html>
